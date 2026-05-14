873d3f2cb91f36255a6cbfdcd535d844a2ea3c4f
Aishwarya Karanth <35144851+AishwaryaKaranth@users.noreply.github.com>
2026-05-14 18:50:02 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        var duplicate=new HashSet<int>();
+        foreach(var num in nums){
+            if(!duplicate.Add(num)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
